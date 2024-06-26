ff7db10e5b10b8408a851251a753fdcd6b62fca2
carlamsa <carlamsaviles@gmail.com>
2024-06-26 15:48:38 +0200

    Menu responsive
    Implementado el menu responsive y responsive del footer en el margin.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,15 @@
-// BURGER MENU
-    const header = document.querySelector('.header')
-    const menu = document.querySelector('.header__menu')
-    const hamburgerMenu = document.querySelector('.header__burger')
-    const lastScrollY = window.scrollY
-    
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > lastScrollY) {
-            // Scroll down
-            header.style.top = '0px' // Ajusta el valor según sea necesario
-            hamburgerMenu.style.display = 'block' // Muestra el ícono de hamburguesa
-            menu.style.display = 'none' // Oculta el menú de navegación
-        } else {
-            // Scroll up
-            header.style.top = '0'
-            if (window.innerWidth <= 768) {
-                hamburgerMenu.style.display = 'block' // Muestra el ícono de hamburguesa para pantallas pequeñas
-            } else {
-                hamburgerMenu.style.display = 'none' // Oculta el ícono de hamburguesa para pantallas grandes
-                menu.style.display = 'flex' // Muestra el menú de navegación para pantallas grandes
-            }
-        }
-        lastScrollY = window.scrollY
-    })
-    /* Cuando hago click en header__burger , header__menu TOGGLE (aparece o desaparece) de 'activo'*/
-    hamburgerMenu.addEventListener('click', () => {
-        menu.classList.toggle('activo')
-    })
+// BURGER MENU 
+const nav = document.querySelector('#nav')
+const abrir = document.querySelector('#abrir')
+const cerrar = document.querySelector('#cerrar')
+const header = document.querySelector('header')
+
+
+    // abrir y cerrar el menú burger
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible')
+})
+
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible')
+})
